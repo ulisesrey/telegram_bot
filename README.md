1f5409e04ed215efc4e62fb91feb1fd85344156e
ulisesrey <ulises.rey.torne@gmail.com>
2025-03-14 12:08:11 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# telegram_bot
+# Telegram Bot
 A telegram bot
+
+This is the public repository for deployment of a [private](https://github.com/ulisesrey/conversacion_papa) one where I am developing the bot.
---
+++ main.py
@@ -0,0 +1,60 @@
+import logging
+import yaml
+from telegram import Update
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
+
+#from huggingface_hub import InferenceClient
+from together import Together
+
+# Read tokens from config YAML
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+    TOKEN = config['TOKEN']
+    T_API_KEY = config['TOGETHER_API']
+
+client = Together(api_key=T_API_KEY)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+# Async functions
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def conversation(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    completion = client.chat.completions.create(
+        model="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
+        messages=[{"role": "user", "content": update.message.text}],
+)
+    response = completion.choices[0].message.content
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+# Send a Good morning message if it is 8:00 AM, send it daily
+async def good_morning(context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=context.job.context, text='Good morning!')
+
+# This handler should be added last
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+
+    # Exclude commands from the conversation handler
+    not_command = ~filters.COMMAND
+    conversation_handler = MessageHandler(filters.TEXT & not_command, conversation)
+
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+
+    application.add_handler(start_handler)
+    application.add_handler(conversation_handler)
+    application.add_handler(unknown_handler)
+    
+    application.run_polling()
